d75ed9869e69ad6a6bcb36d62c29d4ecd13dbe1c
Sofia Morales <sofimoralesroa92@gmail.com>
2024-10-22 11:51:34 +0200

    delete de favoritos

--- routes/favorite.routes.js
+++ routes/favorite.routes.js
@@ -11,9 +11,9 @@ router.post("/", verifyToken, async (req, res) => {
   try {
     const user = await User.findById(req.payload._id);
 
-    if(req.body.productId){
+    if (req.body.productId) {
       user.favorites.products.push(req.body.productId);
-    }else if(req.body.kitId){
+    } else if (req.body.kitId) {
       user.favorites.kits.push(req.body.kitId);
     }
 
@@ -22,6 +22,27 @@ router.post("/", verifyToken, async (req, res) => {
     });
 
     res.status(200).json(updateUser.favorites);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+
+router.delete("/products/:productId", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.payload._id);
+
+    const favoritesFiltered = user.favorites.products.filter(
+      (eachProductId) => eachProductId !== req.params.productId
+    );
+    user.favorites = favoritesFiltered;
+
+    const updateUser = await User.findByIdAndUpdate(req.payload._id, user, {
+      new: true,
+    });
+
+    res.status(204).send();
   } catch (error) {
     console.log(error);
     next(error);
