b030f4c3a1bf28855591a128ac18f8b39e0bd320
Sofia Morales <sofimoralesroa92@gmail.com>
2024-10-24 00:12:18 +0200

    delete de favoritos

--- routes/favorite.routes.js
+++ routes/favorite.routes.js
@@ -29,14 +29,35 @@ router.post("/", verifyToken, async (req, res) => {
 });
 
 
-router.delete("/products/:productId", verifyToken, async (req, res) => {
+router.delete("/products/:productId", verifyToken, async (req, res, next) => {
   try {
     const user = await User.findById(req.payload._id);
 
     const favoritesFiltered = user.favorites.products.filter(
-      (eachProductId) => eachProductId !== req.params.productId
+      (eachProductId) => eachProductId.toString() !== req.params.productId
     );
-    user.favorites = favoritesFiltered;
+    user.favorites.products = favoritesFiltered;
+
+    const updateUser = await User.findByIdAndUpdate(req.payload._id, user, {
+      new: true,
+    });
+
+    res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+
+router.delete("/kits/:kitId", verifyToken, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.payload._id);
+
+    const favoritesFiltered = user.favorites.kits.filter(
+      (eachKitId) => eachKitId.toString() !== req.params.kitId
+    );
+    user.favorites.kits = favoritesFiltered;
 
     const updateUser = await User.findByIdAndUpdate(req.payload._id, user, {
       new: true,
